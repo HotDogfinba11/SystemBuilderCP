a12214669830dce7c63b8f6e94c21044fbb974da
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-06-19 23:07:57 +0100

    Initial Commit

---
+++ Shared/SystemBuilderCPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemBuilderCPApp.swift
+//  Shared
+//
+//  Created by Oliver Harrison on 19/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SystemBuilderCPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
