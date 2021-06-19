53d007d7e85427bc55446a031eb3564c21b9efc9
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-06-19 23:41:24 +0100

    Project: Creation of BuildView
    To have a view callable by BuildView() NavigationLink

---
+++ BuildView.swift
@@ -0,0 +1,21 @@
+//
+//  BuildView.swift
+//  SystemBuilderCP
+//
+//  Created by Oliver Harrison on 19/06/2021.
+//
+
+import SwiftUI
+
+struct BuildView: View {
+    var body: some View {
+        Text("placeholder")
+            .navigationTitle("Build")
+    }
+}
+
+struct BuildView_Previews: PreviewProvider {
+    static var previews: some View {
+        BuildView()
+    }
+}
