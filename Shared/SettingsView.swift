4fa466434c2b2f42708e2ff84b68df97f0c68ed4
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-06-19 23:46:05 +0100

    Project: Creation of BuildView
    To have a view callable by SettingsView() NavigationLink

---
+++ Shared/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  SystemBuilderCP
+//
+//  Created by Oliver Harrison on 19/06/2021.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("placeholder")
+            .navigationTitle("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
