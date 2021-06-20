5d9b7de7d6165492dd6076302e12c8cd648dcc3b
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-06-20 01:07:21 +0100

    Project: Creation of BottomNav
    Foundation for a separate bottom NavBar for iOS

---
+++ iOS/BottomNav.swift
@@ -0,0 +1,26 @@
+//
+//  BottomNav.swift
+//  SystemBuilderCP (iOS)
+//
+//  Created by Oliver Harrison on 20/06/2021.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+  var body: some View {
+    TabView {
+        Text("placeholder")
+            .tabItem {
+                Image(systemName: "macpro.gen3")
+                Text("Builder")
+            }
+     
+        Text("placeholder")
+            .tabItem {
+                Image(systemName: "gear")
+                Text("Settings")
+            }
+    }
+  }
+}
