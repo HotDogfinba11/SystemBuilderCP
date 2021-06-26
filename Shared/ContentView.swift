9b30b729d09ecffca4924a3b188ca8a5126fb313
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-06-26 20:47:27 +0100

    ContentView: Movement of Sidebar toggle to NavigationView
    Inline with how macOS and iPadOS apps deal with the Sidebar toggle, more logical UX

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -35,14 +35,14 @@ struct ContentView: View{
                             }
                         }
                 }
-            }
-        }
-        .toolbar {
-            ToolbarItem(placement: .automatic) {
-                Button(action: {
-                  NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
-                }) {
-                  Image(systemName: "sidebar.left")
+                .toolbar {
+                    ToolbarItem(placement: .automatic) {
+                        Button(action: {
+                          NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+                        }) {
+                          Image(systemName: "sidebar.left")
+                        }
+                    }
                 }
             }
         }
