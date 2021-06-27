851068d72f5fc965897a667ea29cdb31970e6402
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-06-27 19:36:09 +0100

    Changing from SwiftUICharts to LightCharts
    More aesthetically pleasing, cleaner code

--- Shared/BuildView.swift
+++ Shared/BuildView.swift
@@ -6,40 +6,16 @@
 //
 
 import SwiftUI
-import SwiftUICharts
+import LightChart
 
 struct BuildView: View {
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(spacing: 8) {
-                    GroupBox(
-                        label: Label("Pricing", systemImage: "dollarsign.square.fill")
-                            .foregroundColor(.green)
-                    ) {
-                        let price = Legend(color: .yellow, label: "Average")
-                        let points: [DataPoint] = [
-                            
-                            .init(value: 130, label: "1", legend: price),
-                            .init(value: 120, label: "2", legend: price),
-                            .init(value: 100, label: "4", legend: price),
-                            .init(value: 130, label: "5", legend: price),
-                            .init(value: 138, label: "10", legend: price),
-                            .init(value: 150, label: "11", legend: price),
-                            .init(value: 150, label: "13", legend: price),
-                            .init(value: 136, label: "14", legend: price),
-                            .init(value: 130, label: "17", legend: price),
-                            .init(value: 150, label: "18", legend: price),
-                            .init(value: 150, label: "20", legend: price),
-                            .init(value: 160, label: "21", legend: price),
-                            .init(value: 158, label: "24", legend: price),
-                        ]
-                        BarChartView(dataPoints: points)
+                ScrollView{
+                    VStack(spacing: 8) {
+                        GraphView()
                     }
-                    .padding()
-                    .frame(minWidth: 320, maxHeight: 400)
                 }
-            }
             .navigationTitle("Build")
         }
         .navigationTitle("Build")
@@ -50,5 +26,20 @@ struct BuildView: View {
 struct BuildView_Previews: PreviewProvider {
     static var previews: some View {
         BuildView()
+    }
+}
+
+struct GraphView: View {
+    var body: some View {
+        GroupBox(
+            label: Label("Average price over time", systemImage: "dollarsign.square.fill")
+                .foregroundColor(.green)
+        ) {
+            LightChartView(data: [2, 30, 9, 23, 10, 20, 15, 17, 6, 23],
+                           type: .curved,
+                           visualType: .filled(color: .yellow, lineWidth: 5))
+        }
+        .padding()
+        .frame(minWidth: 320, minHeight: 200, maxHeight: 200)
     }
 }
