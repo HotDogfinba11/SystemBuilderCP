6dfac0ed69769fc9fca1f3b08495e2c5f0429889
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-06-27 17:51:20 +0100

    BuildView: Graph added to NavigationView
    Including of GroupBox into a scrollable page

--- Shared/BuildView.swift
+++ Shared/BuildView.swift
@@ -10,34 +10,42 @@ import SwiftUICharts
 
 struct BuildView: View {
     var body: some View {
-        GroupBox(
-                    label: Label("Pricing", systemImage: "dollarsign.square.fill")
-                        .foregroundColor(.green)
-        ) {
-            let price = Legend(color: .yellow, label: "Average")
-
-            let points: [DataPoint] = [
-                .init(value: 130, label: "1", legend: price),
-                .init(value: 120, label: "2", legend: price),
-                .init(value: 100, label: "4", legend: price),
-                .init(value: 130, label: "5", legend: price),
-                .init(value: 138, label: "10", legend: price),
-                .init(value: 150, label: "11", legend: price),
-                .init(value: 150, label: "13", legend: price),
-                .init(value: 136, label: "14", legend: price),
-                .init(value: 130, label: "17", legend: price),
-                .init(value: 150, label: "18", legend: price),
-                .init(value: 150, label: "20", legend: price),
-                .init(value: 160, label: "21", legend: price),
-                .init(value: 158, label: "24", legend: price),
-            ]
-            BarChartView(dataPoints: points)
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 8) {
+                    GroupBox(
+                        label: Label("Pricing", systemImage: "dollarsign.square.fill")
+                            .foregroundColor(.green)
+                    ) {
+                        let price = Legend(color: .yellow, label: "Average")
+                        let points: [DataPoint] = [
+                            
+                            .init(value: 130, label: "1", legend: price),
+                            .init(value: 120, label: "2", legend: price),
+                            .init(value: 100, label: "4", legend: price),
+                            .init(value: 130, label: "5", legend: price),
+                            .init(value: 138, label: "10", legend: price),
+                            .init(value: 150, label: "11", legend: price),
+                            .init(value: 150, label: "13", legend: price),
+                            .init(value: 136, label: "14", legend: price),
+                            .init(value: 130, label: "17", legend: price),
+                            .init(value: 150, label: "18", legend: price),
+                            .init(value: 150, label: "20", legend: price),
+                            .init(value: 160, label: "21", legend: price),
+                            .init(value: 158, label: "24", legend: price),
+                        ]
+                        BarChartView(dataPoints: points)
+                    }
+                    .padding()
+                    .frame(minWidth: 320, maxHeight: 400)
+                }
+            }
+            .navigationTitle("Build")
         }
-        .padding()
         .navigationTitle("Build")
-        .frame(minWidth: 320, maxHeight: 400)
     }
 }
+    
 
 struct BuildView_Previews: PreviewProvider {
     static var previews: some View {
