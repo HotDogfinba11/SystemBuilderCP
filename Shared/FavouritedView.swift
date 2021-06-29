a72e2b89dd8a2bc58f7d188bc53374ae216e6e00
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-06-29 13:34:34 +0100

    FavouritedView: List is now dynamic
    Dynamically add or remove list items, also saves on repeating lines of code unnecessarily

--- Shared/FavouritedView.swift
+++ Shared/FavouritedView.swift
@@ -7,59 +7,39 @@
 
 import SwiftUI
 
+struct PCType: Identifiable {
+    let id: Int
+    let name: String
+    let type: String
+    let price: String
+}
+
 struct FavouritedView: View {
+    @State var PCList = [
+        PCType(id: 0, name: "Nephew's PC", type: "discord", price: "£700"),
+        PCType(id: 1, name: "Grandad's PC", type: "apple", price: "£400"),
+        PCType(id: 2, name: "Daughter's PC", type: "apple", price: "£350"),
+    ]
+    
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink(destination: GraphView()){
-                    HStack{
-                        Image("discord")
+            List(PCList) { pclist in
+                NavigationLink(destination: GraphView()) {
+                    HStack {
+                        Image(pclist.type)
                             .resizable()
                             .scaledToFit()
                             .frame(width: 40, height: 40)
-                    VStack(alignment: .leading, spacing: 5, content: {
-                        Text("Nephew's PC")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                        Text("Gaming PC, price of £700")
-                            .font(.caption)
-                            .foregroundColor(Color.secondary)
-                        
-                    })
+                        VStack(alignment: .leading, spacing: 5, content: {
+                            Text(pclist.name)
+                                .font(.title2)
+                                .fontWeight(.bold)
+                            Text("Gaming PC, price of \(pclist.price)")
+                                .font(.caption)
+                                .foregroundColor(Color.secondary)
+                        })
+                        }
                     }
-                }
-                NavigationLink(destination: GraphView()){
-                    HStack{
-                        Image("apple")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 40, height: 40)
-                    VStack(alignment: .leading, spacing: 5, content: {
-                        Text("Grandad's PC")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                        Text("Desktop PC, price of £400")
-                            .font(.caption)
-                            .foregroundColor(Color.secondary)
-                    })
-                    }
-                }
-                NavigationLink(destination: GraphView()){
-                    HStack{
-                        Image("apple")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 40, height: 40)
-                    VStack(alignment: .leading, spacing: 5, content: {
-                        Text("Daughter's PC")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                        Text("Desktop PC, price of £350")
-                            .font(.caption)
-                            .foregroundColor(Color.secondary)
-                    })
-                    }
-                }
             }
             .listStyle(SidebarListStyle())
             .frame(minWidth: 220)
@@ -68,6 +48,7 @@ struct FavouritedView: View {
         .navigationTitle("Favourites")
     }
 }
+
 
 struct FavouritedView_Previews: PreviewProvider {
     static var previews: some View {
