831fd66e09a90931c5d732aa33ba05e533a4689d
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-09-17 09:30:24 +0100

    SettingsView: OS specific settings
    Instead of using the same function for both OSes, using specific settings for both allows it to feel native for both platforms. This can also allow for OS specific preferences.

--- Shared/SettingsView.swift
+++ Shared/SettingsView.swift
@@ -10,30 +10,46 @@ import SwiftUI
 struct SettingsView: View {
     static var github = "https://github.com/HotDogfinba11/SystemBuilderCP"
     var body: some View {
+        #if os(macOS)
+        TabView {
+            GeneralSettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+        }
+        .frame(width: 500, height: 250)
+        #else
         NavigationView {
-            Form {
-                Section(header: Text("Retailers"),
-                        footer: Text("Disabling retailers will prevent the automatic PC builder from using said retailers")) {
-                    Toggle(isOn: .constant(true),
-                           label: {
-                            Text("Amazon")
-                           })
-                    Toggle(isOn: .constant(true),
-                           label: {
-                            Text("Scan")
-                           })
-                    Toggle(isOn: .constant(true),
-                           label: {
-                            Text("Curry's PC World")
-                           })
-                }
-                Section(header: Text("About"),
-                        footer: Text("A-level programming project")) {
-                    Link("Source code is available on Github", destination: URL(string: "http://github.com/HotDogfinba11/SystemBuilderCP")!)
-                    Label("Version 0.1", systemImage: "info.circle")
-                }
+            GeneralSettingsView()
+                .navigationTitle("Settings")
+        }
+        #endif
+    }
+}
+
+struct GeneralSettingsView: View {
+    var body: some View {
+        Form {
+            Section(header: Text("Retailers"),
+                    footer: Text("Disabling retailers will prevent the automatic PC builder from using said retailers")) {
+                Toggle(isOn: .constant(true),
+                       label: {
+                        Text("Amazon")
+                       })
+                Toggle(isOn: .constant(true),
+                       label: {
+                        Text("Scan")
+                       })
+                Toggle(isOn: .constant(true),
+                       label: {
+                        Text("Curry's PC World")
+                       })
             }
-            .navigationTitle("Settings")
+            Section(header: Text("About"),
+                    footer: Text("A-level programming project")) {
+                Link("Source code is available on Github", destination: URL(string: "http://github.com/HotDogfinba11/SystemBuilderCP")!)
+                Label("Version 0.1", systemImage: "info.circle")
+            }
         }
     }
 }
